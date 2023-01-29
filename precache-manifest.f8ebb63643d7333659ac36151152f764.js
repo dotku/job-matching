01d73f027b706c9b8a23ec4ccb38fcd496a61b14
Weijing Jay Lin <weijingjaylin@gmail.com>
2023-01-29 01:13:49 -0800

    Updates

--- precache-manifest.f8ebb63643d7333659ac36151152f764.js
+++ precache-manifest.f8ebb63643d7333659ac36151152f764.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e1b3259a6174ffdae674d6f9d33531c7",
+    "revision": "063a41c2fe2fd6e914930d2473e3d07e",
     "url": "/react-job-matching/index.html"
   },
   {
-    "revision": "d48d8abd14c9659699f4",
+    "revision": "3d8c7c3118a95c74b363",
     "url": "/react-job-matching/static/css/main.a1b57797.chunk.css"
   },
   {
-    "revision": "b184b10f30c9acfc4887",
-    "url": "/react-job-matching/static/js/2.c45987f0.chunk.js"
+    "revision": "3af6cf7f81fdd955b248",
+    "url": "/react-job-matching/static/js/2.a8bca7f4.chunk.js"
   },
   {
-    "revision": "d48d8abd14c9659699f4",
-    "url": "/react-job-matching/static/js/main.406744f3.chunk.js"
+    "revision": "3d8c7c3118a95c74b363",
+    "url": "/react-job-matching/static/js/main.cf20a499.chunk.js"
   },
   {
     "revision": "a433694cae7feeb29deb",
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-job-matching/precache-manifest.42c23878aa6b580c900294b56aa4c25b.js"
+  "/react-job-matching/precache-manifest.f8ebb63643d7333659ac36151152f764.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-job-matching/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
