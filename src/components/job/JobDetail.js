92e06079bdcbb086ed31bf4cdd32144134e89d99
Weijing Jay Lin <weijingjaylin@gmail.com>
2023-02-02 14:37:36 -0800

    add apply button

--- src/components/job/JobDetail.js
+++ src/components/job/JobDetail.js
@@ -30,9 +30,34 @@ export default function JobDetail() {
   if (isLoading) return <div>Loading ...</div>;
   return (
     <WithErrorContent error={error}>
-      <div>
+      <div className="mb-5">
         <h2>{job.title}</h2>
-        <div dangerouslySetInnerHTML={{ __html: job.content }} />
+        {job.company && (
+          <div>
+            <strong>{job.company}</strong>
+          </div>
+        )}
+        {job.referenceJobID && (
+          <div>
+            <small>{job.referenceJobID}</small>
+          </div>
+        )}
+        <div
+          dangerouslySetInnerHTML={{ __html: job.content }}
+          className="my-3"
+        />
+        {job.contact_name && (
+          <div className="text-center mt-4">
+            {job.contact_email && (
+              <a
+                href={`mailto:${job.contact_email}?subject=Regarding the job ${job.title}&body=%0D%0A%0D%0A%0D%0Asent from job-matching`}
+                className="btn btn-outline-dark"
+              >
+                Apply
+              </a>
+            )}
+          </div>
+        )}
       </div>
     </WithErrorContent>
   );
