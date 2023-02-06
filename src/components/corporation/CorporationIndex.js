4d191c4a4bb2e907400bb252a6cc658ea4dfa5e7
Weijing Jay Lin <weijingjaylin@gmail.com>
2023-02-05 23:25:38 -0800

    add payment input

--- src/components/corporation/CorporationIndex.js
+++ src/components/corporation/CorporationIndex.js
@@ -70,6 +70,34 @@ export default function CorperationIndex({ phrase }) {
           </div>
         </WithErrorContent>
       )}
+      <div className="text-center">
+        <form
+          action="https://www.paypal.com/cgi-bin/webscr"
+          method="post"
+          target="_top"
+        >
+          <input type="hidden" name="cmd" value="_s-xclick" />
+          <input type="hidden" name="hosted_button_id" value="335K4PZWH5ZHJ" />
+          <input
+            type="image"
+            src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif"
+            border="0"
+            name="submit"
+            alt="PayPal - The safer, easier way to pay online!"
+          />
+          <img
+            alt=""
+            border="0"
+            src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
+            width="1"
+            height="1"
+          />
+        </form>
+      </div>
+      {/* <h3>Reference</h3>
+      <a href="https://companiesmarketcap.com/professional-services/largest-professional-service-companies-by-market-cap/">
+        https://companiesmarketcap.com/professional-services/largest-professional-service-companies-by-market-cap/
+      </a> */}
     </div>
   );
 }
