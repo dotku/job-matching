7068373fd9c3002e276e74a691bc4ff4637b5bc2
Weijing Jay Lin <weijingjaylin@gmail.com>
2023-02-08 01:35:25 -0800

    update talent template

--- src/components/Talents.js
+++ src/components/Talents.js
@@ -15,38 +15,46 @@ export function Talents(props) {
       phrase ? talent.name.match(new RegExp(phrase, "i")) : true
     )
     .map((talent, key) => (
-      <div className="col-sm-6 col-md-4 my-2" key={key}>
+      <div className="col-sm-6 my-2" key={key}>
         <div className="card">
           <div className="card-body">
-            <div className="card-title h5">
-              <Link to="/resume">
-                <div className="d-flex flex-row">
-                  <div
-                    style={{
-                      width: "52px",
-                      height: "52px",
-                      lineHeight: "52px",
-                    }}
-                    className="bg-secondary text-light rounded-circle d-inline-block"
-                  >
-                    <div className="d-flex justify-content-center align-items-center">
-                      <i
-                        className="bi bi-person-fill h2 mb-0"
-                        style={{ lineHeight: "52px" }}
-                      ></i>
-                    </div>
-                  </div>
-                  <div className=" ms-2" style={{ lineHeight: "52px" }}>
-                    {talent.name}
+            <div className="card-title mb-0">
+              <div className="d-flex flex-row">
+                <div
+                  style={{
+                    width: "52px",
+                    height: "52px",
+                    lineHeight: "52px",
+                  }}
+                  className="bg-secondary text-light rounded-circle d-inline-block"
+                >
+                  <div className="d-flex justify-content-center align-items-center">
+                    <i
+                      className="bi bi-person-fill h2 mb-0"
+                      style={{ lineHeight: "52px" }}
+                    ></i>
                   </div>
                 </div>
-              </Link>
-            </div>
-            <div className="card-text">
-              <div className="">
-                <span title="Year of Experience">{talent.career_age}yr(s)</span>
-                ,{" "}
-                <span title="Highest Position">{talent.highest_position}</span>
+                <div className="ms-2">
+                  <div className="h5 mb-0">
+                    <Link to="/resume">{talent.name}</Link>
+                    {talent.current_company && (
+                      <>
+                        {` (`}
+                        <Link to={`/coperation/${talent.current_company}`}>
+                          {talent.current_company}
+                        </Link>
+                        {`)`}
+                      </>
+                    )}
+                  </div>
+                  <div>
+                    <span title="Year of Experience">
+                      {talent.career_age}yr(s)
+                    </span>
+                    <span className="ms-1">{talent.highest_position}</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
