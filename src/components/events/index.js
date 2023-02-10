39faba6ac358764dae035e835eeaf0d147e94232
Weijing Jay Lin <weijingjaylin@gmail.com>
2023-02-09 17:06:56 -0800

    queues events

--- src/components/events/index.js
+++ src/components/events/index.js
@@ -1,22 +1,30 @@
+import { useEffect, useState } from "react";
+
 export default function Events() {
-  return (
-    <div className="card mb-3" id="event-card">
-      <a href="https://boeing.avature.net/stlhiringevent">
-        <img
-          className="card-img-top"
-          src="http://clientfiles.tmpwebeng.com/boeing/132397V_Boeing_Apr26_HuntsvilleVirtualCareerEvent_EmailHTML/Header.png"
-          alt="Boeing"
-        />
-      </a>
-      <div className="card-body text-center">
-        <h3 className="card-title">Boeing St. Louis Career Event</h3>
-        <a
-          href="https://boeing.avature.net/stlhiringevent"
-          className="btn btn-primary"
-        >
-          Register
-        </a>
-      </div>
-    </div>
+  const [results, setResults] = useState([]);
+  useEffect(() => {
+    fetch("/react-job-matching/data/events.json")
+      .then((rsp) => rsp.json())
+      .then((rsp) => setResults(rsp))
+      .catch((e) => console.error(e));
+  }, []);
+  return results && results.length ? (
+    results
+      .filter((_item, idx) => idx === 0)
+      .map((item, idx) => (
+        <div className="card mb-3" id="event-card" key={idx}>
+          <a href={item.url}>
+            <img className="card-img-top" src={item.imgSrc} alt={item.title} />
+          </a>
+          <div className="card-body text-center">
+            <h3 className="card-title">{item.title}</h3>
+            <a href={item.url} className="btn btn-primary">
+              Register
+            </a>
+          </div>
+        </div>
+      ))
+  ) : (
+    <div>Empty Content</div>
   );
 }
