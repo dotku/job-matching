ef5dd559f77f9c2db3a8c29ca96a989281657f05
Weijing Jay Lin <weijingjaylin@gmail.com>
2023-02-17 14:20:56 -0800

    add build report data

---
+++ src/components/corporation/CorporationRank/index.jsx
@@ -0,0 +1,90 @@
+export default function Blind2022() {
+// Rank by Men
+// Rank by Women
+// Company
+
+// 1
+// 1
+// Square
+
+// 2
+// 3
+// NVIDIA
+
+// 3
+// 2
+// Netflix
+
+// 4
+// 17
+// Robinhood
+
+// LO
+// 5
+// LO
+// 5
+// Google
+
+// 6
+// 7
+// Spotify
+
+// 7
+// 19
+// Airbnb
+
+// 8
+// 16
+// Adobe
+
+// 9
+// 14
+// Salesforce
+
+// 10
+// 11
+// Indeed
+
+// 11
+// 13
+// Twilio
+
+// 1227
+// 133
+// 15
+// Meta
+
+// 6
+// CO
+// Stripe
+
+// 14
+// 20
+// DoorDash
+
+// 155
+// 8
+// LinkedIn
+
+// 166
+// 4
+// ServiceNow
+
+// 17
+// 18
+// Shopify
+
+// 18
+// 12
+// Pinterest
+
+// 19
+// 9
+// Microsoft
+
+// 20
+// 10
+// Apple
+
+}
+
