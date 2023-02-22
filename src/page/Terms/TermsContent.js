9b40431502f77994fad07432f0f5f338e18c2aeb
Weijing Jay Lin <weijingjaylin@gmail.com>
2023-02-21 21:42:03 -0800

    add statistic information on H-1B applications

--- src/page/Terms/TermsContent.js
+++ src/page/Terms/TermsContent.js
@@ -3,10 +3,16 @@ export function TermsContent() {
     <dl>
       <dt>H-1B</dt>
       <dd>
-        The H-1B visa is a nonimmigrant work visa that allows U.S. employers to
-        hire foreign workers for specialty jobs that require a bachelor’s degree
-        or equivalent. This can include occupations in fields such as IT,
-        finance, engineering, architecture or more.
+        <p>
+          The H-1B visa is a nonimmigrant work visa that allows U.S. employers
+          to hire foreign workers for specialty jobs that require a
+          bachelor&apos;s degree or equivalent. This can include occupations in
+          fields such as IT, finance, engineering, architecture or more.
+        </p>
+        <p>
+          There are 65,000 H1B visas approved each year and an extra 20,000 for
+          the cap.
+        </p>
       </dd>
       <dt>OPT</dt>
       <dd>
