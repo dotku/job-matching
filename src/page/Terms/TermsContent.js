85d5f2669a63638a13824680d787da20b9b2a5b8
Weijing Jay Lin <weijingjaylin@gmail.com>
2023-02-21 21:29:47 -0800

    add H1B term

--- src/page/Terms/TermsContent.js
+++ src/page/Terms/TermsContent.js
@@ -1,6 +1,13 @@
 export function TermsContent() {
   return (
     <dl>
+      <dt>H-1B</dt>
+      <dd>
+        The H-1B visa is a nonimmigrant work visa that allows U.S. employers to
+        hire foreign workers for specialty jobs that require a bachelor’s degree
+        or equivalent. This can include occupations in fields such as IT,
+        finance, engineering, architecture or more.
+      </dd>
       <dt>OPT</dt>
       <dd>
         F-1 students who have maintained their status and have completed one
